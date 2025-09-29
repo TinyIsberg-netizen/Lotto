540ada75995c0c1b6ea7c507ac8fc8692e5c360d
Filip <isberget23@gmail.com>
2025-09-29 11:05:16 +0200

    new file:   randomNumb.java

---
+++ randomNumb.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.*;
+
+public class randomNumb {
+    int randomNumb = (int)(Math.random() * 35) + 1;
+    public int getRandomNumb() {
+        return randomNumb;
+    }
+
+    
+    
+
+    
+
+}
